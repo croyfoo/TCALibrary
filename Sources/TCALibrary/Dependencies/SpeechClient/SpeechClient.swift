d5b9dc21acd77745002ae6e19e24346d1c61269e
David Croy <djcroy@mac.com>
2025-02-24 14:53:35 -0600

    added public to the key

--- Sources/TCALibrary/Dependencies/SpeechClient/SpeechClient.swift
+++ Sources/TCALibrary/Dependencies/SpeechClient/SpeechClient.swift
@@ -62,7 +62,7 @@ extension SpeechClient: TestDependencyKey {
 }
 
 extension DependencyValues {
-  var speechClient: SpeechClient {
+  public var speechClient: SpeechClient {
     get { self[SpeechClient.self] }
     set { self[SpeechClient.self] = newValue }
   }
