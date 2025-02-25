ec9e8b3514fe4b734f005591c1bf8c33da990923
David Croy <djcroy@mac.com>
2025-02-25 13:56:10 -0600

    updated download to have a paramter to show the progress.

--- Sources/TCALibrary/Dependencies/DownloadClient/DownloadClient.swift
+++ Sources/TCALibrary/Dependencies/DownloadClient/DownloadClient.swift
@@ -3,7 +3,9 @@ import Foundation
 
 @DependencyClient
 public struct DownloadClient: @unchecked Sendable {
-  public var download: @Sendable (_ url: URLRequest ) -> AsyncThrowingStream<Event, Error> = { _ in .finished() }
+  public var download: @Sendable (_ url: URLRequest, _ showProgress: Bool ) -> AsyncThrowingStream<Event, Error> = {
+    _,_ in .finished()
+  }
   
   @CasePathable
   public enum Event: Equatable {
@@ -21,17 +23,17 @@ extension DependencyValues {
 
 extension DownloadClient: DependencyKey {
   public static let liveValue = Self(
-    download: { request in
+    download: { request, showProgress in
       AsyncThrowingStream<Event, Error> (bufferingPolicy: .unbounded) { continuation in
         Task {
           do {
             let (bytes, response) = try await URLSession.shared.bytes(for: request)
-            var data = Data()
+            var data     = Data()
             var progress = 0
             for try await byte in bytes {
               data.append(byte)
-              let newProgress = Int(
-                Double(data.count) / Double(response.expectedContentLength) * 100)
+              guard showProgress else { continue }
+              let newProgress = Int( Double(data.count) / Double(response.expectedContentLength) * 100)
               if newProgress != progress {
                 progress = newProgress
                 continuation.yield(.updateProgress(Double(progress) / 100))
@@ -48,4 +50,10 @@ extension DownloadClient: DependencyKey {
   )
   
   public static let testValue = Self()
+}
+
+extension DownloadClient {
+  public func download( _ url: URLRequest, showProgress: Bool = false ) -> AsyncThrowingStream<Event, Error> {
+    self.download(url, showProgress)
+  }
 }
