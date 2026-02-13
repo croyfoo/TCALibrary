bc3a3eeca91772547294dcbb45907ed5888b2f24
David Croy <djcroy@mac.com>
2026-02-13 15:55:56 -0600

    Use CommonLib instead of DDSCommon

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMajor(from: "1.23.0")),
-    .package(url: "https://github.com/croyfoo/DDSCommon", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/DoubleDogSoftware/CommonLib", .upToNextMajor(from: "1.0.0")),
 //    .package(url: "https://github.com/pointfreeco/swift-dependencies", .upToNextMajor(from: "1.6.0")),
 //    .package(url: "https://github.com/pointfreeco/swift-gen", from: "0.3.1"),
 //    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.4")
@@ -29,7 +29,7 @@ let package = Package(
       name: "TCALibrary",
       dependencies: [
         .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-        .product(name: "DDSCommon", package: "DDSCommon"),
+        .product(name: "CommonLib", package: "CommonLib"),
         // Add any other product dependencies here
       ]
     ),
